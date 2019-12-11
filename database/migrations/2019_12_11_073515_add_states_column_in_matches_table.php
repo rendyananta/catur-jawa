e7dcb3c8c64f6423fc46d5815b8bfdaf0aa38080
Rendy Ananta <rendyananta66@gmail.com>
2019-12-11 16:09:10 +0700

    add winner checking

---
+++ database/migrations/2019_12_11_073515_add_states_column_in_matches_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStatesColumnInMatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('matches', function (Blueprint $table) {
+            $table->json('states')->default(null)->after('accepted');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('matches', function (Blueprint $table) {
+            $table->dropColumn('states');
+        });
+    }
+}
